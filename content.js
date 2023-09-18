681c89c0c7ded98a6e41ebdaeda91b9dc58097b9
Parth <parthp15@yahoo.com>
2023-09-17 19:44:43 -0700

    final version-- Record time of completion: 15.5 s

--- content.js
+++ content.js
@@ -11,33 +11,33 @@ function main() {
         setTimeout(function() {
             document.getElementById('QID14-1-label').click();
             NextButton();
-        }, 4000); 
+        }, 3500); 
 
         setTimeout(function() {
             document.getElementById('QID15-5-label').click();
             NextButton();
-        }, 8000); 
+        }, 4400); 
         
         setTimeout(function() {
             document.getElementById("QR~QID80").value = "more friendly";
             NextButton();
-        }, 12000); 
+        }, 5400); 
         
         setTimeout(function() {
             document.getElementById('QID18-5-label').click();
             NextButton();
-        }, 16000); 
+        }, 6100); 
 
         setTimeout(function() {
             document.getElementById('QID19-5-label').click();
             NextButton();
             
-        }, 20000); 
+        }, 6800); 
 
         setTimeout(function() {
             document.getElementById('QID20-5-label').click();
             NextButton();
-        }, 24000); 
+        }, 7800); 
 
         setTimeout(() => {
             document.querySelector('input[id="QR~QID23~4~2"]').checked = true;
@@ -47,37 +47,37 @@ function main() {
             document.querySelector('input[id="QR~QID23~10~2"]').checked = true;
             document.querySelector('input[id="QR~QID23~11~2"]').checked = true;
             NextButton();
-        }, 28000)
+        }, 8500)
 
         setTimeout(() =>{
             NextButton();
-        }, 30000)
+        }, 9300)
 
         setTimeout(() =>{
             document.querySelector('input[id="QR~QID44~1~2"]').checked = true;
             document.querySelector('input[id="QR~QID44~3~2"]').checked = true;
             NextButton();
-        }, 34000)
+        }, 10200)
 
         setTimeout(() =>{
             document.getElementById('QID37-2-label').click();
             NextButton();
-        }, 38000)
+        }, 11200)
         
         setTimeout(() =>{
             document.getElementById('QID48-4-label').click();
             NextButton();
-        }, 42000)
+        }, 11900)
 
         setTimeout(() =>{
             document.getElementById('QID54-52-label').click();
             NextButton();
-        }, 44000)
+        }, 12700)
 
         setTimeout(() =>{
             document.getElementById('QID57-2-label').click();
             NextButton();
-        }, 48000)
+        }, 13400)
 
         setTimeout(() => {
             document.querySelector('input[id="QR~QID59~8~2"]').checked = true;
@@ -89,17 +89,17 @@ function main() {
             document.querySelector('input[id="QR~QID59~6~1"]').checked = true;
             document.querySelector('input[id="QR~QID59~7~2"]').checked = true;
             NextButton();
-        }, 52000)
+        }, 14200)
 
         setTimeout(() => {
             document.getElementById('QID74-5-label').click();
             NextButton();
-        }, 56000)
+        }, 15000)
 
         setTimeout(() => {
             document.getElementById("QR~QID79").value = "all good";
             NextButton();
-        }, 60000)
+        }, 15500)
     }
 }
 
